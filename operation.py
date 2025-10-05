6964192367aaa902111347511b7a6f54896ca232
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-10-05 13:02:46 +0530

    Add initial operation.py file

---
+++ operation.py
@@ -0,0 +1,13 @@
+
+# Initialize two numbers
+num1 = 10
+num2 = 5
+
+# Addition
+sum_result = num1 + num2
+
+# Subtraction
+sub_result = num1 - num2
+
+print(f"Addition of {num1} and {num2} is: {sum_result}")
+print(f"Subtraction of {num2} from {num1} is: {sub_result}")
